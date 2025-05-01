d5392c15eb4cd0b3c1db0eda2339360862bd1f86
saipranith57 <saipranith57@gmail.com>
2025-05-01 09:44:33 +0530

    changes

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { CONTACT } from "../constants/index.js";
-import Resume from "../assets/SaiPranith Elaprolu Mern Stack Resume";
+import Resume from "../assets/Resume.pdf";
 
 const ContactUs = () => {
   return (
